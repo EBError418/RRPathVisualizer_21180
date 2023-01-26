ffce154a42c41c074dbaf3b422bc640d4af403cd
ftc21180 <ftc21180@gmail.com>
2023-01-25 20:40:43 -0600

    update sdk to 19

--- src/main/kotlin/TrajectoryGen.kt
+++ src/main/kotlin/TrajectoryGen.kt
@@ -4,32 +4,80 @@ import com.acmerobotics.roadrunner.trajectory.Trajectory
 import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder
 import com.acmerobotics.roadrunner.trajectory.constraints.DriveConstraints
 import com.acmerobotics.roadrunner.trajectory.constraints.MecanumConstraints
+import java.lang.Thread.sleep
 
 object TrajectoryGen {
     // Remember to set these constraints to the same values as your DriveConstants.java file in the quickstart
-    private val driveConstraints = DriveConstraints(60.0, 60.0, 0.0, 270.0.toRadians, 270.0.toRadians, 0.0)
+    private val driveConstraints = DriveConstraints(52.0, 52.0, 0.0, 250.0.toRadians, 250.0.toRadians, 0.0)
 
     // Remember to set your track width to an estimate of your actual bot to get accurate trajectory profile duration!
-    private const val trackWidth = 16.0
+    private const val trackWidth = 12.0
 
     private val combinedConstraints = MecanumConstraints(driveConstraints, trackWidth)
 
-    private val startPose = Pose2d(-48.0, -48.0, 90.0.toRadians)
+    private val startPose = Pose2d(-72.0 + 7.0, -36.0, -90.0.toRadians)
 
     fun createTrajectory(): ArrayList<Trajectory> {
         val list = ArrayList<Trajectory>()
 
         val builder1 = TrajectoryBuilder(startPose, startPose.heading, combinedConstraints)
 
-        builder1.forward(40.0);
+//        builder1.splineTo(Vector2d(-48.0, -36.0), -45.0.toRadians)
+
 
         // Small Example Routine
-//        builder1
-//            .splineTo(Vector2d(10.0, 10.0), 0.0)
-//            .splineTo(Vector2d(15.0, 15.0), 90.0);
-
+        builder1
+            .strafeLeft(24.0)
+            .splineToSplineHeading(Pose2d(-24.0, -36.0, -90.0.toRadians), 0.0)
+            //.splineToSplineHeading(Pose2d(-42.0, -42.06, 135.0.toRadians), -45.0.toRadians)
+            //.splineTo(Vector2d(-48.0 + 6, -36.0 - 6), -45.0.toRadians)
+            //.splineTo(Vector2d(-24.0  + 2, -36.0 + 5.6), 90.0.toRadians + 20.0.toRadians)
+            .splineToSplineHeading(Pose2d(-24.0  + 2, -36.0 + 5.6, -90.0.toRadians + 20.0.toRadians), 90.0.toRadians + 0.0.toRadians);
         list.add(builder1.build())
 
+        for (i in 0 until 4) {
+            val start2Pose = Pose2d(-24.0 + 2, -36.0 + 5.6, -90.0.toRadians + 20.0.toRadians)
+            val builder2 = TrajectoryBuilder(start2Pose, startPose.heading, combinedConstraints)
+            builder2
+                .lineToLinearHeading(Pose2d(-12.0, -62.0, -90.0.toRadians));
+
+            sleep(500);
+            val start3Pose = Pose2d(-12.0, -62.0, -90.0.toRadians)
+            val builder3 = TrajectoryBuilder(start3Pose, startPose.heading, combinedConstraints)
+            builder3
+                .lineToLinearHeading(start2Pose);
+            sleep(500);
+
+            list.add(builder2.build())
+            list.add(builder3.build())
+
+        }
+
+        // high junction for 5th cone
+        val start2Pose = Pose2d(-24.0 + 2, -36.0 + 5.6, -90.0.toRadians + 20.0.toRadians)
+        val builder2 = TrajectoryBuilder(start2Pose, startPose.heading, combinedConstraints)
+        builder2
+            .lineToLinearHeading(Pose2d(-12.0, -62.0, -90.0.toRadians));
+        sleep(500);
+
+        val start3Pose = Pose2d(-12.0, -62.0, -90.0.toRadians)
+        val hjPose = Pose2d(-2.0, -36.0+5.6, -110.0.toRadians)
+        val builder3 = TrajectoryBuilder(start3Pose, startPose.heading, combinedConstraints)
+        builder3
+            .lineToLinearHeading(hjPose);
+        sleep(500);
+
+        list.add(builder2.build())
+        list.add(builder3.build())
+
+        // parking
+        val parkPose = Pose2d(-36.0, -36.0, -180.0.toRadians)
+
+        val builder4 = TrajectoryBuilder(hjPose, startPose.heading, combinedConstraints)
+        builder4
+            .lineToLinearHeading(parkPose);
+
+        list.add(builder4.build())
         return list
     }
 
